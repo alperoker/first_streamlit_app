d271b544be63337e52fa53644164df37f1fddae7
alperoker <106712735+alperoker@users.noreply.github.com>
2023-02-16 22:25:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,6 @@ if streamlit.button('Get Fruit Load List'):
     streamlit.dataframe(my_data_rows)
 
 
-my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', my_fruit)
+
 
 
